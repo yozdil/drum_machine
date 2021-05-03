9702a12b01b9221b830b10eed776043829aa86a9
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-03 14:15:12 -0400

    set sound bank toggle segment

--- src/App.js
+++ src/App.js
@@ -7,21 +7,34 @@ import {
   Checkbox,
   Divider,
   Segment,
+  Grid,
 } from "semantic-ui-react";
+import { useState } from "react";
 
 function App() {
 
+  const [bank, setBank] = useState(true)
+
+  if (bank) {
+    
+  }
   const data = Banks().bankOne;
 
   return (
     <div className="container">
       <Container id="drum-machine">
-        <Segment>
-          <Checkbox
-            onChange={() => console.log("Implement state to switch banks")}
-            slider
-          />
-        </Segment>
+        <Segment.Group horizontal>
+          <Segment textAlign="center">
+            <Header>Switch Sound Bank</Header>
+            <Checkbox
+              onChange={() => console.log("Implement state to switch banks")}
+              slider
+            />
+          </Segment>
+          <Segment>Middle</Segment>
+          <Segment>Bottom</Segment>
+        </Segment.Group>
+
         <Divider />
         <Container id="display">
           <Header as="h1">Drum Machine</Header>
