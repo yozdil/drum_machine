374dc6b1585700595a67c2e6b2a753789eba9a3f
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-03 11:30:08 -0400

    initialize project, clean up setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/app.scss";
+import { Header, Container } from "semantic-ui-react";
+import Drumpad from "./components/Drumpad";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container id="drum-machine">
+        <Container id="display">
+          <Header>Hello</Header>
+          <Drumpad />
+        </Container>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Drumpad.js
@@ -0,0 +1,8 @@
+const Drumpad = () => {
+  return (
+    <h1>Drumpad</h1>
+  )
+}
+
+
+export default Drumpad;
