b0b7d0b1a58744be7d70d43f2515d8b8f9b169d8
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-06 10:40:05 -0400

    remove app style folder and clean up

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import DrumMachine from "./components/DrumMachine";
 import Bank from "./components/Bank";
 import DB from "./helpers/db";
-import "./styles/app.scss";
 import { Header, Container, Divider, Segment } from "semantic-ui-react";
 import { useState } from "react";
 
 function App() {
   const [bank, setBank] = useState(DB().bankOne);
-
   const [display, setDisplay] = useState("Welcome!");
 
   return (
     <div className="container">
       <Container id="drum-machine">
-        <Segment.Group horizontal>
+        <Segment.Group vertical>
           <Bank setDisplay={setDisplay} setBank={setBank} />
-          <Segment clearing id="display" textAlign="center">
+          <Segment
+            inverted
+            color="green"
+            clearing
+            id="display"
+            textAlign="center"
+          >
             <Header as="h1">Drum Machine</Header>
             <Header as="h2">{display}</Header>
           </Segment>
         </Segment.Group>
-
         <Divider />
-        <Container>
-          <DrumMachine setDisplay={setDisplay} data={bank} />
-        </Container>
+        <Segment padded="very">
+          <Container className="machine-container">
+            <DrumMachine setDisplay={setDisplay} data={bank} />
+          </Container>
+        </Segment>
+        <a href="https://yamacozdil.com/">
+          <Header floated="right" color="green" as="h4">
+            by Yamac Ozdil
+          </Header>
+        </a>
       </Container>
     </div>
   );
